c85423a4a061b1553c8d3ac6c8043088ddfc4f74
sagarnaik965 <97589195+sagarnaik965@users.noreply.github.com>
2024-12-04 15:19:05 +0530

    Update test_query.lua

--- test_query.lua
+++ test_query.lua
@@ -1,24 +1,26 @@
-sysbench.hooks.report_intermediate = function()
-end
+local report_interval = 5 -- Set the interval for intermediate reports
+local last_report_time = os.time()
+local test_start_time = os.time() -- Capture the start time of the test
+local query_count = 0 -- Track the number of queries performed
 
 function thread_init()
-    db_conn = sysbench.sql.driver():connect()
+   -- Initialize thread-level variables or connections here
 end
 
 function thread_done()
-    db_conn:disconnect()
+   -- Clean up resources
 end
 
 function event()
-    db_conn:query([[
-        SELECT 
-            u.username AS user,
-            SUM(o.quantity) AS total_quantity
-        FROM 
-            orders o
-        JOIN 
-            users u ON o.user_id = u.user_id
-        GROUP BY 
-            u.username;
-    ]])
+   -- Perform your query
+   db_query("SELECT u.username AS user, SUM(o.quantity) AS total_quantity FROM orders o JOIN users u ON o.user_id = u.user_id GROUP BY u.username;")
+   query_count = query_count + 1
+
+   -- Check if it's time to report
+   local now = os.time()
+   if now - last_report_time >= report_interval then
+       last_report_time = now
+       local elapsed_time = now - test_start_time
+       print(string.format("[ %ds ] queries performed: %d", elapsed_time, query_count))
+   end
 end
